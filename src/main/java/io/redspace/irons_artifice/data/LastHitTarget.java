2aba3f037b10d9d4268f2f6b811120fcbea4a9a6
iron431 <34083081+iron431@users.noreply.github.com>
2026-08-30 13:05:15 -0500

    chain modifier onhit->onposthit

--- src/main/java/io/redspace/irons_artifice/data/LastHitTarget.java
+++ src/main/java/io/redspace/irons_artifice/data/LastHitTarget.java
@@ -1,22 +1,22 @@
 package io.redspace.irons_artifice.data;
 
 import io.redspace.irons_artifice.registry.DataAttachmentRegistry;
-import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.LivingEntity;
 import org.jspecify.annotations.Nullable;
 
 import java.util.UUID;
 
 /**
- * Transient, per-player record of last living entity hit with a bullet, for consecutive-hit tracking.
+ * Transient, record of last living entity hit with a bullet, for consecutive-hit tracking.
  */
 public record LastHitTarget(@Nullable UUID uuid) {
     public static final LastHitTarget NONE = new LastHitTarget(null);
 
-    public static LastHitTarget get(ServerPlayer player) {
-        return player.getData(DataAttachmentRegistry.LAST_HIT_TARGET);
+    public static LastHitTarget get(LivingEntity entity) {
+        return entity.getData(DataAttachmentRegistry.LAST_HIT_TARGET);
     }
 
-    public static void set(ServerPlayer player, @Nullable UUID uuid) {
-        player.setData(DataAttachmentRegistry.LAST_HIT_TARGET, uuid == null ? NONE : new LastHitTarget(uuid));
+    public static void set(LivingEntity entity, @Nullable UUID uuid) {
+        entity.setData(DataAttachmentRegistry.LAST_HIT_TARGET, uuid == null ? NONE : new LastHitTarget(uuid));
     }
 }
--- src/main/java/io/redspace/irons_artifice/modifier/modifiers/ChainShotModifier.java
+++ src/main/java/io/redspace/irons_artifice/modifier/modifiers/ChainShotModifier.java
@@ -1,25 +1,31 @@
 package io.redspace.irons_artifice.modifier.modifiers;
 
+import io.redspace.irons_artifice.data.ParticleStack;
 import io.redspace.irons_artifice.data.ShotComponentMap;
 import io.redspace.irons_artifice.data.ShotComponents;
+import io.redspace.irons_artifice.entity.ChainEntity;
 import io.redspace.irons_artifice.modifier.GunModifier;
 import io.redspace.irons_artifice.modifier.on_hit_handlers.ChainShotOnHit;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.particles.BlockParticleOption;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.level.block.Blocks;
 
 import java.util.function.Consumer;
 
 public final class ChainShotModifier implements GunModifier {
     @Override
     public void apply(ShotComponentMap components) {
-        components.getOrCreate(ShotComponents.ON_HIT).add(new ChainShotOnHit());
-//        components.getOrCreate(ShotComponents.PARTICLE_TRAIL).addAccent(
-//                new ParticleStack.ParticleAccent(new BlockParticleOption(ParticleTypes.BLOCK_CRUMBLE, Blocks.IRON_CHAIN.defaultBlockState()), 0.5)
-//        );
+        components.getOrCreate(ShotComponents.POST_HIT_EFFECTS).add(new ChainShotOnHit());
+        components.getOrCreate(ShotComponents.PARTICLE_TRAIL).addAccent(
+                new ParticleStack.ParticleAccent(new BlockParticleOption(ParticleTypes.BLOCK_CRUMBLE, Blocks.IRON_CHAIN.defaultBlockState()), 0.5)
+        );
     }
 
     @Override
     public void getDescriptionText(Consumer<Component> builder) {
         builder.accept(Component.translatable("irons_artifice.modifier.chain_shot").withStyle(ChatFormatting.AQUA));
+        builder.accept(Component.translatable("irons_artifice.tooltip.max_range", ChainEntity.SPAWN_RANGE).withStyle(ChatFormatting.AQUA));
     }
 }
